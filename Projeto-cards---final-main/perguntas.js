cc59ff9381111abecbf51a06d87ea4cc3c6b7c32
vitorhdutra <150350614+vitorhdutra@users.noreply.github.com>
2024-11-09 21:09:27 -0300

    Add files via upload

---
+++ Projeto-cards---final-main/perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+criaCartao(
+    'Ciências',
+    'Qual é o metal mais leve na tabela períodica?',
+    'O metal mais leve é o lítio'
+)
+criaCartao(
+    'Ciências',
+    'Qual é o único mamífero que pode voar?',
+    'O único mamífero que pode voar é o morcego'
+)
+criaCartao(
+    'Curiosidades',
+    'Qual é a língua mais falada no mundo?',
+    'O mandarim é a língua mais falada, considerando o número de falantes nativos'
+)
+criaCartao(
+    'História',
+    'Qual é a capital da Austrália?',
+    'É Canberra'
+)
+criaCartao(
+    'Ciência',
+    'Qual é o maior planeta do Sistema Solar?',
+    'È Júpiter'
+)
